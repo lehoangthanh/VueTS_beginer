0d805760988d4a40cdff79eabbb137a6b9523614
lehoangthanhit <lehoangthanhit@gmail.com>
2022-04-13 14:37:05 +0700

    remove btn utility add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
